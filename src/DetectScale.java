333f60627c5e91307f4339ce59398ff7bb1ea437
Atesman <andrewates67@gmail.com>
2024-04-14 12:56:48 -0500

    Added scales to dictionary

--- src/DetectScale.java
+++ src/DetectScale.java
@@ -16,6 +16,12 @@ public class DetectScale {
 
 	    //HEXATONIC
 	    	
+			scaleDictionary.put("100110011001", new String[]{"Augmented", ""});
+			scaleDictionary.put("101110010100", new String[]{"Major Blues", ""});
+			scaleDictionary.put("100101110010", new String[]{"Minor Blues", ""});
+			scaleDictionary.put("101011010100", new String[]{"Major Hexatonic", ""});
+			scaleDictionary.put("101101010010", new String[]{"Minor Hexatonic", ""});
+			scaleDictionary.put("101010100110", new String[]{"Prometheus", "Mystic chord: 1-#4-b7-3-6-2"});
 	    	scaleDictionary.put("101010101010", new String[]{"Whole Tone", ""});
 
 	    //HEPTATONIC
@@ -29,10 +35,30 @@ public class DetectScale {
 	        scaleDictionary.put("101101011010", new String[]{"Natural Minor", "Aeolian - 6th Mode of the Major Scale"});
 	        scaleDictionary.put("110101101010", new String[]{"Locrian", "7th Mode of the Major Scale"});
 	        
+	        //Harmonic Minor
+	        scaleDictionary.put("101101011001", new String[]{"Harmonic Minor", ""});
+	        scaleDictionary.put("110101100110", new String[]{"Locrian Natural 6", "2nd Mode of the Harmonic Minor Scale"});
+	        scaleDictionary.put("101011001101", new String[]{"Ionian #5", "3rd Mode of the Harmonic Minor Scale"});
+	        scaleDictionary.put("101100110110", new String[]{"Dorian #11", "4th Mode of the Harmonic Minor Scale / aka Ukrainian Dorian or Romanian Minor"});
+	        scaleDictionary.put("110011011010", new String[]{"Phyrgian Dominant", "5th Mode of the Harmonic Minor Scale"});
+	        scaleDictionary.put("100110110101", new String[]{"Lydian #2", "6th Mode of the Harmonic Minor Scale"});
+	        scaleDictionary.put("110110101100", new String[]{"Altered Diminished", "7th Mode of the Harmonic Minor Scale / aka Super Locrian bb7"});
+	        
+	        //Melodic Minor
+	        scaleDictionary.put("101101010101", new String[]{"Melodic Minor", ""});
+	        scaleDictionary.put("110101010110", new String[]{"Dorian b9", "2nd Mode of the Melodic Minor Scale"});
+	        scaleDictionary.put("101010101101", new String[]{"Lydian Augmented", "3rd Mode of the Melodic Minor Scale"});
+	        scaleDictionary.put("101010110110", new String[]{"Lydian Dominant", "4th Mode of the Melodic Minor Scale / aka Acoustic or Overtone"});
+	        scaleDictionary.put("101011011010", new String[]{"Mixolydian b6", "5th Mode of the Melodic Minor Scale"});
+	        scaleDictionary.put("101101101010", new String[]{"Aeolian b5", "6th Mode of the Melodic Minor Scale"});
+	        scaleDictionary.put("110110101010", new String[]{"Altered", "7th Mode of the Melodic Minor Scale / aka Altered Dominant, Super Locrian, or Diminished Whole Tone"});
+	        	
 	    //OCTATONIC
 	        
 	        scaleDictionary.put("101101101101", new String[]{"Diminished", "Whole-Half Diminished"});
 	        scaleDictionary.put("110110110110", new String[]{"Dominant Diminished", "Half-Whole Diminished"});
+	        scaleDictionary.put("101011011101", new String[]{"Major Sixth Diminished", "Barry Harris's Sixth Diminished Scale"});
+	        scaleDictionary.put("101101011101", new String[]{"Minor Sixth Diminished", "Barry Harris's Sixth Diminished Scale"});
 	             
 	    //DODECATONIC
 
